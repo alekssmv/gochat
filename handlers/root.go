8ee901c7334f0ff75fe787c810d09abd1ec56c65
Александр Смирнов <alexx5314@gmail.com>
2024-05-25 02:51:29 +0300

    Фронт

--- handlers/root.go
+++ handlers/root.go
@@ -1,10 +1,10 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html><body><h1>Welcome to the homepage!</h1></body></html>")
+
+	http.ServeFile(w, r, "front/index.html")
 }
